b1aadbe0df1bec37735223d190aada5eb4d4957a
Fkorsrud <126823227+Fkorsrud@users.noreply.github.com>
2023-03-02 15:13:40 -0600

    first demo

---
+++ movingrectangle/sketch.js
@@ -0,0 +1,16 @@
+// first day of vs code
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  rect(mouseX, 100, 200, 50);
+}
