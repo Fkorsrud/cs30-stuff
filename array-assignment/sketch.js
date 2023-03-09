cf9fb89a937d7f3b945aea362fc31fb68d8eeac4
Fkorsrud <126823227+Fkorsrud@users.noreply.github.com>
2023-03-09 13:04:29 -0600

    worked on arrays

--- array-assignment/sketch.js
+++ array-assignment/sketch.js
@@ -5,13 +5,53 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let circles = [];
+let maxDiameter;
+let sizeChange = 50;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  addBoxes();
+  translate(width/2, height/2);
 }
 
 function draw() {
   background(220);
+  for (let i =0; i < circles.length; i++){
+    displayCircles(circles[i]);
+  }
 }
 
 
+function spawnCircle(diaMeter){
+  let someCirle = {
+    d: diaMeter,
+    r: diaMeter/2,
+  };
+  circles.push(someCirle);
+
+}
+
+function addBoxes() {
+  if (width >= height){
+    maxDiameter = width;
+  }
+  else{
+    maxDiameter = height;
+  }
+  for (let i = maxDiameter; i > 5; i-= sizeChange){
+    spawnCircle(i);
+  }
+}
+
+function displayCircles(theCircle){
+  let numberofPoints = theCircle.d / 5;
+  beginShape();
+  for (let i = numberofPoints; i> 0; i--){
+    push();
+    rotate(i *(360/numberofPoints));
+    curveVertex(0, 0 + theCircle.diaMeter);
+    pop();
+  }
+  endShape();
+}
