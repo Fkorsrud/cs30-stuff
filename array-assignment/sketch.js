245a78578110f3ffb799b718f3dac5631de88023
Fkorsrud <126823227+Fkorsrud@users.noreply.github.com>
2023-03-18 12:56:16 -0600

    i broke it

--- array-assignment/sketch.js
+++ array-assignment/sketch.js
@@ -55,7 +55,7 @@ function addCircles() {
 function displayCircles(theCircle){
   let numberofPoints = theCircle.d/points;
   let angle = 360/numberofPoints;
-  
+  translate(width/2, height/2);
   noStroke();
   fill(theCircle.red, theCircle.green, theCircle.blue);
   beginShape();
@@ -111,7 +111,7 @@ function keyPressed(){
 }
 
 function displayAllcircles(){
-  translate(width/2, height/2);
+  // translate(width/2, height/2);
   for (let i =0; i < circles.length; i++){
      displayCircles(circles[i]);
   }
