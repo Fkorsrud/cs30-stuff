6bbe596287d3aed75ef63c3a5757b2ea862732e8
Fkorsrud <126823227+Fkorsrud@users.noreply.github.com>
2023-03-19 15:06:53 -0600

    Update sketch.js

--- array-assignment/sketch.js
+++ array-assignment/sketch.js
@@ -1,6 +1,6 @@
 // Arrays and object notation
 // Faith
-// Date
+// march 20th, 2023
 //
 // Extra for Experts:
 // I made custom shapes.
@@ -13,11 +13,12 @@ let points = 5;
 let difference = 5;
 
 function setup() {
+  // setup the things we need to setup
   angleMode(DEGREES);
   createCanvas(windowWidth, windowHeight);
-  //translate(width/2, height/2);
   background(random(255), random(255), random(255));
   
+  // add all the circles, then displays them
   addCircles();
   displayAllcircles();
   
@@ -28,6 +29,7 @@ function draw() {
 }
 
 function spawnCircle(diaMeter){
+  // make the circle object
   let someCirle = {
     d: diaMeter,
     r: diaMeter/2,
@@ -35,18 +37,24 @@ function spawnCircle(diaMeter){
     green: random(255),
     blue: random(255),
   };
+
+  // push it to the list
   circles.push(someCirle);
 }
 
 function addCircles() {
+  // set the max diameter to be the highest of width and height
   if (width >= height){
     maxDiameter = width;
   }
   else{
     maxDiameter = height;
   }
+  // add a circles starting with the biggest one
   for (let i = maxDiameter; i > 5; i-= sizeChange){
     spawnCircle(i);
+
+    // once the diameter is small then 100, make the difference in size smaller
     if (i > 100){
       sizeChange = i*2/10;
     }
@@ -54,22 +62,29 @@ function addCircles() {
 }
 
 function displayCircles(theCircle){
+// declare variables and set up the fill and stroke
   let numberofPoints = theCircle.d/points;
   let angle = 360/numberofPoints;
-  // translate(width/2, height/2);
   noStroke();
   fill(theCircle.red, theCircle.green, theCircle.blue);
-  translate(width/2, height/2);
+  
+  // begin the shape
   beginShape();
+
+  // to start the curves, the first two and last two need to be the same. 
   let starty = random(theCircle.r/difference);
   curveVertex(0, theCircle.r+ starty);
   curveVertex(0, theCircle.r + starty);
-  for (let i = numberofPoints; i > 0; i--){
+
+  // use trig to put a curve vertex for every point on the 
+  for (let i = numberofPoints -1; i > 0; i--){
     let r = theCircle.r + random(theCircle.r/difference);
     let xValue = sin(angle*i) * r;
     let yValue = cos(angle*i) * r;
     curveVertex(xValue, yValue);
   }
+
+  // put the last two points and end the shape
   curveVertex(0, theCircle.r + starty);
   curveVertex(0, theCircle.r + starty);
   endShape();
@@ -77,34 +92,47 @@ function displayCircles(theCircle){
 
 
 function keyPressed(){
-  
+
+  // detect what key is pressed
   if (keyCode === UP_ARROW){
-    if (points >= 3){
-      points --;
-      displayAllcircles();
-    }
+
     if(points < 3 && points > 0.1){
       points = points - 0.1;
       displayAllcircles();
     }
-  }
-  if (keyCode === DOWN_ARROW){
-    if (points < 15){
-      points ++;
+
+    if (points >= 3){
+      points --;
       displayAllcircles();
     }
+  }
+
+
+  else if (keyCode === DOWN_ARROW){
+
     if (points < 3){
       points = points + 0.1;
       displayAllcircles();
     }
+
+    if (points < 15 && points >3){
+      points ++;
+      displayAllcircles();
+    }
   }
-  if (keyCode === RIGHT_ARROW){
+
+
+  else if (keyCode === RIGHT_ARROW){
+
     if (difference > 0){
       difference --;
       displayAllcircles();
     }
   }
-  if (keyCode === LEFT_ARROW){
+
+
+  else if (keyCode === LEFT_ARROW){
+
     if (difference < 15){
       difference ++;
       displayAllcircles();
@@ -113,7 +141,12 @@ function keyPressed(){
 }
 
 function displayAllcircles(){
+  // translate the middle of the circle to the middle of the canvas
+  push();
+  translate(width/2, height/2);
+  // display every circle
   for (let i =0; i < circles.length; i++){
      displayCircles(circles[i]);
   }
+  pop();
 }
